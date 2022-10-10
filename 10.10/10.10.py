670fead6edb75da4919fc323ba321344d3adc230
NataliaZueva <91422734+NataliaZueva@users.noreply.github.com>
2022-10-10 10:54:09 +0800

    new

--- 10.10/10.10.py
+++ 10.10/10.10.py
@@ -5,31 +5,13 @@ def file_read(title):
         a.append([int(x) for x in line.split()])
     return a
 
+
 a = file_read('text.txt')
 
-
-v = 4
 b = []
-for i in range(v):
-    b.append([0 for i in range(v)])
-print(b)
+for i in range(a[0][0]):
+    b.append([0 for i in range(a[0][0])])
 
 for i in range(1, len(a)):
-    v = a[i]
-    print(v)
-    b[v[0]-1][v[1]-1] = 1
+    b[(a[i])[0] - 1][(a[i])[1] - 1] = 1
 print(b)
-
-
-
-# print(a[0])
-# # def pr():
-#
-# b = []
-# for i in range(a[0][0]):
-#     for j in range(a[0][0]):
-#         if a[i+1][]
-#         b.append(0)
-#     b.append("\n")
-#
-# print(b)
