7ad50623297b2d757edb982834835a8b3676f1f8
NataliaZueva <91422734+NataliaZueva@users.noreply.github.com>
2022-10-17 15:09:59 +0800

    17.10

--- 17.10/17.10.py
+++ 17.10/17.10.py
@@ -1,8 +1,9 @@
 f = open("text.txt")
 s = open('xyz.txt','w')
-m = ['\t', ' ', '\n', '    ', '		', '		', '\t\t']
+m = ['\t', ' ', '\n', '		', '		', '\t\t', '		']
 for line in f.readlines():
-    if not (m and line.startswith("	#") or line.startswith("#")):
+    if not (m and line.startswith("	#") or line.startswith("#") or  line.startswith("		#")):
         s.write(str(line))
         print(line)
-c.close()
+
+s.close()
