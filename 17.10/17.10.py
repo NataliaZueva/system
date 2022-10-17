f8f903a1fc806b2963a310ee4194f3a3945a7358
NataliaZueva <91422734+NataliaZueva@users.noreply.github.com>
2022-10-17 14:55:04 +0800

    17.10

---
+++ 17.10/17.10.py
@@ -0,0 +1,8 @@
+f = open("text.txt")
+s = open('xyz.txt','w')
+m = ['\t', ' ', '\n', '    ', '		', '		', '\t\t']
+for line in f.readlines():
+    if not (m and line.startswith("	#") or line.startswith("#")):
+        s.write(str(line))
+        print(line)
+c.close()
